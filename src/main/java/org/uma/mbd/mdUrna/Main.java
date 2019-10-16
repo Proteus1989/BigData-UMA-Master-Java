d8564f5ab251ee6cddf6b3634d3d709613b621ef
Antonio Suárez <antonioproteus@gmail.com>
2019-10-16 20:19:07 +0200

    Gestionando excepciones en Urnas

--- src/main/java/org/uma/mbd/mdUrna/Main.java
+++ src/main/java/org/uma/mbd/mdUrna/Main.java
@@ -14,22 +14,36 @@ public class Main {
         }*/
 
         // Ejercicio
-        int nb = Integer.parseInt(args[0]);
-        int nn = Integer.parseInt(args[1]);
 
-        System.out.println("Numero de bolas blancas: " + nb);
-        System.out.println("Numero de bolas negras: " + nn);
+        try {
+            int nb = Integer.parseInt(args[0]);
+            int nn = Integer.parseInt(args[1]);
 
-        Urna u = new Urna(nb,nn);
-        while(u.totalBolas() > 1){
-            Urna.ColorBola b1 = u.extraerBola();
-            Urna.ColorBola b2 = u.extraerBola();
-            if(b1==b2)
-                u.ponerBlanca();
-            else
-                u.ponerNegras();
+            System.out.println("Numero de bolas blancas: " + nb);
+            System.out.println("Numero de bolas negras: " + nn);
+
+            Urna u = new Urna(nb, nn);
+            while (u.totalBolas() > 1) {
+                Urna.ColorBola b1 = u.extraerBola();
+                Urna.ColorBola b2 = u.extraerBola();
+                if (b1 == b2)
+                    u.ponerBlanca();
+                else
+                    u.ponerNegras();
+            }
+
+            System.out.println(u.extraerBola());
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("Error en la entrada de datos. Introduce 2 argumentos");
+        } catch (NumberFormatException e)
+        {
+            System.err.println("Algun dato no es numerico " + e.getMessage());
+        } catch (IllegalArgumentException e)
+        {
+            System.err.println(e.getMessage());
+        } finally {
+            System.err.println("No se como habra ido pero adios");
         }
 
-        System.out.println(u.extraerBola());
     }
 }
